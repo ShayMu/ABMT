9d70355a7376407a9f69df4859888b02ff9c55e5
Shay Mualem <shaymu01@gmail.com>
2020-09-12 12:04:02 +0300

    added instructions

--- code.js
+++ code.js
@@ -1,46 +1,44 @@
+let instructionsDiv = document.getElementById('instructions');
+
+let activityDiv = document.getElementById('activity');
 let plusDiv = document.getElementById('plus');
 let picturesDiv = document.getElementById('pictures');
 let probeDiv = document.getElementById('probes');
-let topProbe = document.getElementById('topProbe');
-let botProbe = document.getElementById('botProbe');
-let startButton = document.getElementById('startButton');
+let topProbeDiv = document.getElementById('topProbe');
+let botProbeDiv = document.getElementById('botProbe');
+
+let midPauseDiv = document.getElementById('midPause');
+let finishDiv = document.getElementById('finish');
 
 let mouseClicked = null;
 let probeLocation = null;
 
-let isStarted = false;
-const numberOfImages = 20
-const numOfRepetInHalf = 10
+const numberOfImages = 20;
+const numOfRepeats = 4;
 
-showStep(0);
+showInstructions();
 
 function start() {
-
-    document.body.style.cursor = 'none';
-    isStarted = true;
     loopIt();
 }
 
 async function loopIt() {
     let delay = 500;
+    showActivity();
 
-    for (let i=0 ; i<numOfRepetInHalf ; i++) {
+    for (let i=0 ; i < numOfRepeats / 2 ; i++) {
         await fullCircle(delay);
     }
-    startButton.innerText = "Well done.\n You Done half.\n\n Click here when you are ready to continue."
-    document.body.style.cursor = 'none';
-    showStep(0);
+
+    showMidPause();
     await waitForResponse();
-    startButton.innerText = ''
-    for (let i=0 ; i<numOfRepetInHalf ; i++) {
+    showActivity();
+
+    for (let i=0 ; i < numOfRepeats / 2 ; i++) {
         await fullCircle(delay);
     }
-    startButton.innerHTML = "Well done.\n You're done."
-    startButton.style.cursor = 'none';
-    showStep(0);
-    await waitForResponse();
-    startButton.disabled = true;
-    isStarted = false;
+
+    finish();
 }
 
 async function fullCircle(delay){
@@ -60,41 +58,35 @@ async function fullCircle(delay){
 function setProbe() {
     let content = getRndInteger(0, 2) == 0? '>' : '<';
 
-    topProbe.innerHTML = '';
-    botProbe.innerHTML = '';
+    topProbeDiv.innerHTML = '';
+    botProbeDiv.innerHTML = '';
 
-    if (probeLocation == 'top') topProbe.innerHTML = content;
-    if (probeLocation == 'bot') botProbe.innerHTML = content;
+    if (probeLocation == 'top') topProbeDiv.innerHTML = content;
+    if (probeLocation == 'bot') botProbeDiv.innerHTML = content;
 }
 
 function showStep(step) {
-
-    if (step == 0) startButton.style.display = '';
-    else startButton.style.display = 'none';
-
     switch (step) {
-        case 0: {
-            plusDiv.style.display = 'none';
-            picturesDiv.style.display = 'none';
-            picturesDiv.style.opacity = '0';
-            probeDiv.style.display = 'none';
-        } break;
+        // plus
         case 1: {
             plusDiv.style.display = '';
             picturesDiv.style.display = '';
             picturesDiv.style.opacity = '0';
             probeDiv.style.display = 'none';
         } break;
+        // pictures
         case 2: {
             plusDiv.style.display = 'none';
             picturesDiv.style.opacity = '1';
             probeDiv.style.display = 'none';
         } break;
+        // probe
         case 3: {
             plusDiv.style.display = 'none';
             picturesDiv.style.display = 'none';
             probeDiv.style.display = '';
         } break;
+        // blank
         case 4: {
             plusDiv.style.display = 'none';
             picturesDiv.style.display = 'none';
@@ -103,8 +95,47 @@ function showStep(step) {
     }
 }
 
+function showInstructions() {
+    showMouse();
+    activityDiv.style.display = 'none';
+    midPauseDiv.style.display = 'none';
+    finishDiv.style.display = 'none';
+    instructionsDiv.style.display = '';
+}
+
+function showActivity() {
+    hideMouse();
+    activityDiv.style.display = '';
+    midPauseDiv.style.display = 'none';
+    finishDiv.style.display = 'none';
+    instructionsDiv.style.display = 'none';
+}
+
+function showMidPause() {
+    showMouse();
+    activityDiv.style.display = 'none';
+    midPauseDiv.style.display = '';
+    finishDiv.style.display = 'none';
+    instructionsDiv.style.display = 'none';
+}
+
+function finish() {
+    showMouse();
+    activityDiv.style.display = 'none';
+    midPauseDiv.style.display = 'none';
+    finishDiv.style.display = '';
+    instructionsDiv.style.display = 'none';
+}
+
+function showMouse() {
+    document.body.style.cursor = '';
+}
+
+function hideMouse() {
+    document.body.style.cursor = 'none';
+}
+
 function setMouseClick(btn) {
-    if (!isStarted) start();
     mouseClicked = btn;
 }
 
@@ -115,8 +146,8 @@ async function waitForResponse() {
             await sleep(100);
         }
         resolve(mouseClicked);
-        topProbe.innerHTML = '';
-        botProbe.innerHTML = '';
+        topProbeDiv.innerHTML = '';
+        botProbeDiv.innerHTML = '';
     });
 }
 
