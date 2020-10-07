b7e8e46afdcf053a138191f62a13b8b3331727aa
Shay Mualem <shaymu01@gmail.com>
2020-10-07 18:24:53 +0300

    cors support

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,17 @@
 const functions = require('firebase-functions');
+const db = require('./db/db.index');
+const cors = require('cors')({origin: true});
 
 exports.healthCheck = functions.https.onRequest((req, res) => {
   return res.send("I'm here to serve");
 });
 
 exports.saveSessionInfo = functions.https.onRequest((req, res) => {
-  let { email, session, date } = req.body;
+  cors(req, res, async () => {
+    let { email, session } = req.body;
 
-  return res.send(JSON.stringify({email, session, date}));
+    await db.sessions.addSession({email, session});
+
+    return res.send(JSON.stringify({email}));
+  });
 });
